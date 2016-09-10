095f7a5b45249fb0e12fe7579f1acbe54d6d74f7
ansonK <ansonkelly@gmail.com>
2016-09-10 15:30:30 +0100

    Added functional specs to test output for help text and unrecognised command error message

--- spec/functional/draw_example_image_spec.rb
+++ spec/functional/draw_example_image_spec.rb
@@ -39,4 +39,33 @@ OWOOO
       expect { bitmap_editor.process_input 'S' }.to output(second_expected_pattern).to_stdout
     end
   end
+
+  describe 'Inputting an unrecognised command' do
+    it 'prints out an error message' do
+      bitmap_editor = Editor.new
+
+      expect { bitmap_editor.process_input 'ABC' }.to output("Error: Unrecognised command\n").to_stdout
+    end
+  end
+
+  describe 'Wanting to see the help' do
+    let(:help_text) {
+      <<-HEREDOC
+Help:
+
+I M N       - Create a new M x N image with all pixels coloured white (O).
+C           - Clears the table, setting all pixels to white (O).
+L X Y C     - Colours the pixel (X,Y) with colour C.
+V X Y1 Y2 C - Draw a vertical segment of colour C in column X between rows Y1 and Y2 (inclusive).
+H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive).
+S           - Show the contents of the current image
+X           - Terminate the session
+      HEREDOC
+    }
+    it 'prints out the help text' do
+      bitmap_editor = Editor.new
+
+      expect { bitmap_editor.process_input '?' }.to output(help_text).to_stdout
+    end
+  end
 end
